a8692929ee898e2c2c3a6d0e3744b3b3decef463
Michael Easter <codetojoy@gmail.com>
2017-08-19 08:43:32 -0300

    incremental change as part of a larger frenzy of activity

--- functions/monad_functions.php
+++ functions/monad_functions.php
@@ -12,15 +12,15 @@ use Widmogrod\Monad\State as s;
 // something going on my understanding of the currying mechanism,
 // so I'm having trouble removing it. Ugh.
 
-function updateState($prizeCard, $gameState, $dummy) {
+function updateState($prizeCard, $gameState) {
     $newGameState = $gameState->playRound($prizeCard);
 
     return [$prizeCard, $newGameState];
 }
 
 function buildGameState($prizeCard, $dummy = []) { 
-    return s\state(function($gameState) use ($prizeCard, $dummy) {
-        return updateState($prizeCard, $gameState, $dummy);
+    return s\state(function($gameState) use ($prizeCard) {
+        return updateState($prizeCard, $gameState);
     }); 
 } 
 
@@ -34,20 +34,21 @@ function playRound($prizeCard, $dummy = []) {
 // something going on my understanding of the currying mechanism,
 // so I'm having trouble removing it. Ugh.
 
-function logState($prizeCard, $gameState, $dummy) {
+function logState($gameState) {
     // TODO: this should really be using an IO monad
     $gameState->logState();
+    $unused = -1;
 
-    return [$prizeCard, $gameState];
+    return [$unused, $gameState];
 }
 
-function buildLog($prizeCard, $dummy = []) { 
-    return s\state(function($gameState) use ($prizeCard, $dummy) {
-        return logState($prizeCard, $gameState, $dummy);
+function buildLog($prizeCard, $dummy) {
+    return s\state(function($gameState) { 
+        return logState($gameState);
     }); 
 } 
 
-function logGameState($prizeCard = -1, $dummy = []) { 
+function logGameState($prizeCard, $dummy = []) {
     return f\curryN(2, 'buildLog')(...func_get_args()); 
 } 
 
